290bdc08fc9cb6085741e0e7833ea5a1d77ab82a
MinhongShen <761591766@qq.com>
2018-09-11 22:31:05 +0800

    上传

--- src/test/java/driverss/驱动封装.java
+++ src/test/java/driverss/驱动封装.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 /**
  * Created by XULE on 2018/9/8.
  */
-public class testdriver extends seleniumdriver{
+public class 驱动封装 extends seleniumdriver{
     @Test
     public  static  void open(){
         seleniumdriver.open("firefox",driver);
