e9e9db5d037ccb16e5065f76560fdd5e06a5d459
John Doe <john@sample.com>
2018-09-12 16:24:59 +0800

    添加类以及方法

--- src/main/java/actions/Actions.java
+++ src/main/java/actions/Actions.java
@@ -1,8 +1,28 @@
 package actions;
 
+import driver.findelement.Element;
+import driverss.seleniumdriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by Administrator on 2018/9/12.
  */
-public class Actions {
+public class Actions  extends seleniumdriver{
+    //打开网址
+    public static void get(String url){
+        driver.get(url);
+    }
+    //点击
+    public static void  click(By by){
+        Element.findelement(by).click();
+    }
+    //文本框输入方法
+    public static void sendText(By by,String text){
+       WebElement ef= Element.findelement(by);
+        ef.clear();
+        ef.sendKeys(text);
+    }
 
 }
