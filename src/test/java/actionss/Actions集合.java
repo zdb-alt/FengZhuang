55bae1ab03eb604dc3287d9f733ac46492c38ebe
MinhongShen <761591766@qq.com>
2018-09-16 18:14:57 +0800

    更新代码

--- src/test/java/actionss/Actions集合.java
+++ src/test/java/actionss/Actions集合.java
@@ -1,13 +1,12 @@
 package actionss;
 
 
-import actions.Actions;
+import actions.Actionss;
 import driverss.seleniumdriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-import java.sql.Driver;
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2018/9/12.
@@ -16,9 +15,31 @@ public class Actions集合 {
     @Test
     public  void click(){
         seleniumdriver.open("chrome");
-        Actions.get("http://baidu.com");
-        Actions.sendText(By.id("kw"),"selenium");
-        Actions.click(By.id("su"));
+        Actionss.get("http://baidu.com");
+        Actionss.sendText(By.id("kw"),"selenium");
+        Actionss.click(By.id("su"));
         seleniumdriver.quit();
+    }
+    @Test
+    //获取文本
+    public void gettext(){
+         seleniumdriver.open("chrome");
+        Actionss.get("http://baidu.com");
+        String text= Actionss.gettext(By.name("tj_trnews"));
+        System.out.print(text);
+        seleniumdriver.quit();
+    }
+    //获取多个文本
+    @Test
+    public void gettexts(){
+        seleniumdriver.open("chrome");
+        Actionss.get("https://baidu.com");
+        ArrayList list= Actionss.gettexts(By.xpath(".//*[@id='u1']/a"));
+      //  System.out.print(list.get(1));
+        for (int i = 0; i <list.size() ; i++) {
+            System.out.print(list.get(i));
+        }
+        seleniumdriver.quit();
+
     }
 }
